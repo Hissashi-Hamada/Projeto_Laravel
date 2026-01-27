9ae16a674ae6a7dc68d2d5dea790f5d179985f17
enzo <enzohhamada@gmail.com>
2026-01-27 17:12:58 -0300

    modelos dos cards de vendas

--- resources/views/vendas/index.blade.php
+++ resources/views/vendas/index.blade.php
@@ -15,7 +15,8 @@
 </head>
 <body>
 
-    
+
+
 
     <script>
         (function(){
@@ -37,7 +38,64 @@
             });
         })();
     </script>
-
     @yield('scripts')
+    <div class="container py-4">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+
+                <div class="card bg-white text-dark shadow-sm border-0">
+                    <div class="card-body">
+
+                        <h2 class="card-title mb-3">Produto</h2>
+
+                        <img
+                            src=""
+                            alt="Imagem do produto"
+                            class="img-fluid rounded mb-3"
+                        >
+
+                        <p class="card-text mb-3">Descrição</p>
+
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-bold fs-5">Preço</span>
+                            <button type="button" class="btn btn-primary">
+                                Vender
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+
+                <div class="card bg-white text-dark shadow-sm border-0">
+                    <div class="card-body">
+
+                        <h2 class="card-title mb-3">Produto</h2>
+
+                        <img
+                            src=""
+                            alt="Imagem do produto"
+                            class="img-fluid rounded mb-3"
+                        >
+
+                        <p class="card-text mb-3">Descrição</p>
+
+                        <div class="d-flex justify-content-between align-items-center">
+                            <span class="fw-bold fs-5">Preço</span>
+                            <button type="button" class="btn btn-primary">
+                                Vender
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
 </body>
 </html>
